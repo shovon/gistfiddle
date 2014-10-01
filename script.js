2edee282f572ece42e64c654ac66851ce21f567c
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-10-01 13:23:41 -0700

    Able to listen to URL changes.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-if (/^\/[a-z\-0-9]+\/[0-9a-f]+$/i.test(window.location.pathname)) {
-  var id = window.location.pathname.split('/')[2];
-  var $li = $(document.createElement('li'));
+function addFiddleButton() {
+  if (/^\/[a-z\-0-9]+\/[0-9a-f]+$/i.test(window.location.pathname)) {
+    var id = window.location.pathname.split('/')[2];
+    var $li = $(document.createElement('li'));
 
-  var $button = $(document.createElement('a'));
-  $button.addClass('minibutton');
-  $button.attr('href', 'http://jsfiddle.net/gh/gist/library/pure/' + id + '/');
-  $button.attr('target', '_blank');
-  $button.html('JSFiddle');
+    if ($('.pagehead-actions .jsfiddle').length) {
+      return;
+    }
 
-  $li.append($button);
-  $('.pagehead-actions').append($li);
+    var $button = $(document.createElement('a'));
+    $button.addClass('minibutton jsfiddle');
+    $button.attr('href', 'http://jsfiddle.net/gh/gist/library/pure/' + id + '/');
+    $button.attr('target', '_blank');
+    $button.html('JSFiddle');
+
+    $li.append($button);
+    $('.pagehead-actions').append($li);
+  }
 }
+
+addFiddleButton();
+setInterval(addFiddleButton, 100);
