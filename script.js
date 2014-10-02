bd2936a6043e3de81701f4863600b5e50640438e
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-10-01 20:30:57 -0700

    Only show button for Fiddles.

--- script.js
+++ script.js
@@ -19,21 +19,32 @@
 
 function addFiddleButton() {
   if (/^\/[a-z\-0-9]+\/[0-9a-f]+$/i.test(window.location.pathname)) {
-    var id = window.location.pathname.split('/')[2];
-    var $li = $(document.createElement('li'));
-
     if ($('.pagehead-actions .jsfiddle').length) {
       return;
     }
 
-    var $button = $(document.createElement('a'));
-    $button.addClass('minibutton jsfiddle');
-    $button.attr('href', 'http://jsfiddle.net/gh/gist/library/pure/' + id + '/');
-    $button.attr('target', '_blank');
-    $button.html('<span class="icon-jsfiddle octicon"></span> jsFiddle');
+    var isFiddle = false;
 
-    $li.append($button);
-    $('.pagehead-actions').append($li);
+    $('.files .file .info.file-name .permalink .file-name')
+      .each(function (i, e) {
+        if (/^fiddle\.(html|css|js)$/.test($(e).html())) {
+          isFiddle = true;
+        }
+      });
+
+    if (isFiddle) {
+      var id = window.location.pathname.split('/')[2];
+      var $li = $(document.createElement('li'));
+      var $button = $(document.createElement('a'));
+      $button.addClass('minibutton jsfiddle');
+      $button.attr('href', 'http://jsfiddle.net/gh/gist/library/pure/' + id + '/');
+      $button.attr('target', '_blank');
+      $button.html('<span class="icon-jsfiddle octicon"></span> jsFiddle');
+
+      $li.append($button);
+      $('.pagehead-actions').append($li);
+    }
+
   }
 }
 
